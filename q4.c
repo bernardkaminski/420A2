c23a012e803b4c1840b92d2cb709e209ed12a9f2
Bernie <bernie@berniePC.(none)>
2016-10-24 11:23:58 -0400

    v1

--- q4.c
+++ q4.c
@@ -6,7 +6,7 @@
 void process(char* input_filename, char* output_filename)
 {
 	unsigned error;
-	unsigned char *image, *new_image;
+	unsigned char *image, *new_image,*block;
 	unsigned width, height;
 
 	error = lodepng_decode32_file(&image, &width, &height, input_filename);
@@ -18,6 +18,7 @@ void process(char* input_filename, char* output_filename)
 	unsigned char value;
 	int xyz = 123; // more computations
 	MPI_Init(Null, Null);
+	int size;
 
     // Get the number of processesu
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -27,34 +28,44 @@ void process(char* input_filename, char* output_filename)
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	
-	int offset = rank * set_size;
+	int position_in_global = rank * set_size;
+	block = malloc(width * set_size * 4 * sizeof(unsigned char));
 
     
-	for (int i = 0; i < (offset+set_size) ; i++)
+	for (int i = 0; i < set_size ; i++)
 	{
 		for (int j = 0; j < width; j++)
 		{
 			// printf("thread num: %d\n", omp_get_num_threads());
 
 			// printf("thread num: %d\n", omp_get_thread_num());
-			int offset = 4 * width * i + 4 * j;
-			int r = image[offset + 0]; // R
-			rec(r, (offset + 0), new_image);
-			int g = image[offset + 1]; // G
-			rec(g, offset + 1, new_image);
-			int b = image[offset + 2]; // B
-			rec(b, offset + 2, new_image);
-			int a = image[offset + 3]; // A
-			new_image[offset + 3] = a;
-			xyz = pow(sin(i + xyz), cos(xyz + j));
+			int offset_in_image = 4 * width * (position_in_global+i) + 4 * j;
+			int offset= 4 * width * i + 4 * j;
+			int r = image[offset_in_image]; // R
+			rec(r, (offset), block);
+			int g = image[offset_in_image + 1]; // G
+			rec(g, offset + 1, block);
+			int b = image[offset_in_image + 2]; // B
+			rec(b, offset + 2, block);
+			int a = image[offset_in_image + 3]; // A
+			block[offset + 3] = a;
+			//xyz = pow(sin(i + xyz), cos(xyz + j));
 		}
 	}
 	
 	//recieve all the results
 	 int val = 0;
-	 if (rank == 1) {
+	 if (rank == 0) {
         // receive value of x
-        MPI_Recv(&val, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		int blk_size=(set_size*width*4 * sizeof(unsigned char));
+	    memcpy(new_image,block,blk_size);
+		for (int var = 1; var < size; ++var) {
+			char* blk;
+			MPI_Recv(&blk, 1, MPI_INT, var, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			memcpy(new_image+(blksize*var),blk,blk_size);
+		}
+
+        new
         /*
         MPI_Recv(
             void* data,
@@ -67,7 +78,7 @@ void process(char* input_filename, char* output_filename)
         */
     }
 	else {
-		 MPI_Send(&x, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
+		 MPI_ISend(&block, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD);
 	}
 	
 	MPI_Finalize();
